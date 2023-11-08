5d6223a4107609fb10ed9362a26df1e1e1ae1a57
Zurab Vashakidze <zurab.vashakidze@gmail.com>
2023-11-08 14:23:43 +0400

    Tridiagonal matrix algorithm (Thomas algorithm)
    This script function solves a tridiagonal system of linear equations. The coefficient matrix of the system has nonzero elements only on the main diagonal. Additionally, all entries along the sub-diagonal (the first diagonal below the main diagonal) and the supra-diagonal (the first diagonal above the main diagonal) are set to unity.
    
    Co-Authored-By: Jemal Rogava <148445890+jemal-rogava@users.noreply.github.com>

---
+++ Octave_Code/tridiagonal_algotirhm.m
@@ -0,0 +1,20 @@
+function sol = tridiagonal_algotirhm (b, phi)
+  ## determines len
+  len = length (b);
+  ## preallocates solution vector
+  sol = zeros (1, len);
+  ## forward elimination
+  for i = 2:len
+    temp = 1 / b (i - 1);
+    b (i) = b (i) - temp;
+    phi (i) = phi (i) - phi (i - 1) * temp;
+  endfor
+  clear ('i')
+  ## backward substitution
+  sol (len) = phi (len) / b (len);
+  for i = (len - 1):(-1):1
+    sol (i) = (phi (i) - sol (i + 1)) / b (i);
+  endfor
+  clear ('i')
+endfunction
+
