fbe555d435ef78e83c7db8282ebb293455c5ad34
Zurab Vashakidze <zurab.vashakidze@gmail.com>
2023-11-13 18:08:16 +0400

    Adding a relative error of the function q(t)
    Co-Authored-By: Jemal Rogava <148445890+jemal-rogava@users.noreply.github.com>

--- Octave_Code/main_script_kirchhoff_prob.m
+++ Octave_Code/main_script_kirchhoff_prob.m
@@ -14,17 +14,17 @@ T   = 1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 disp('Please input numbers, e.g. 1, 2, ...');
-problem = 3;
+problem = 2;
 run(sprintf('%s%d.m','Test',problem));
 % Exact_Test
 % Test1
 % Test2
 
 %%% Division numbers of the temporal and the spatial intervals %%%
-m = 128; % Division number of the spatial interval %
+m = 64; % Division number of the spatial interval %
 ##n = 2;
-n = T * (m / ell); % Division number of the temporal interval %
-##n = T * (m / ell) * (m / ell); % Division number of the temporal interval %
+##n = T * (m / ell); % Division number of the temporal interval %
+n = T * (m / ell) * (m / ell); % Division number of the temporal interval %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%% Discretization of the temporal interval %%%
@@ -54,10 +54,11 @@ clear i
 val_u      = zeros(length(t),length(x)); % exact values of u(x,t) %
 app_u      = zeros(length(t),length(x)); % approximate values of u_k(x) %
 
-q      = []; %%% q integral %%%
-CondN2 = []; %%% Condition Number of Matrix %%%
+q       = []; %%% q integral %%%
+q_exact = zeros(n + 1,1);
+CondN2  = []; %%% Condition Number of Matrix %%%
 %%% maximum error %%%
-err = zeros(length(t),1);
+err     = zeros(length(t),1);
 
 %%% Tridiagonal system coefficients %%%
 b   = zeros(1,length(x) - 2); % diagonal entries %
@@ -71,48 +72,50 @@ while k <= n + 1
   switch k
     case 1
       app_u(k,:) = psi0(x);
-      tempdiff1 = FinDiff (h, app_u(k,:));
-      tempdiff1 = tempdiff1 .* tempdiff1;
+      tempdiff1  = FinDiff (h, app_u(k,:));
+      tempdiff1  = tempdiff1 .* tempdiff1;
       %%% q integral %%%
-      q1     = alpha(t(k)) + beta(t(k)) * SimpsRule (h, tempdiff1);
-      condN2 = NaN; %%% Condition Number %%%
-      q      = [q;q1];
-      CondN2 = [CondN2;condN2]; %%% Condition Number %%%
+      q1         = alpha(t(k)) + beta(t(k)) * SimpsRule (h, tempdiff1);
+      q_exact(k) = alpha(t(k)) + beta(t(k)) * intdu(t(k));
+      condN2     = NaN; %%% Condition Number %%%
+      q          = [q;q1];
+      CondN2     = [CondN2;condN2]; %%% Condition Number %%%
       val_u(k,:) = u(x,t(k));
-      err(k) = max(abs(app_u(k,:) - val_u(k,:)));
+      err(k)     = max(abs(app_u(k,:) - val_u(k,:)));
     case 2
-      % app_u(k,:) = u(x,t(k));
       app_u(k,:) = app_u(k - 1,:) + tau * (psi1(x) + 0.5 * tau * ...
       (f(x,t(k - 1)) + q1 * d2psi0));
-      tempdiff1 = FinDiff (h, app_u(k,:));
-      tempdiff1 = tempdiff1 .* tempdiff1;
+      tempdiff1  = FinDiff (h, app_u(k,:));
+      tempdiff1  = tempdiff1 .* tempdiff1;
       %%% q integral %%%
-      q2     = alpha(t(k)) + beta(t(k)) * SimpsRule (h, tempdiff1);
+      q2         = alpha(t(k)) + beta(t(k)) * SimpsRule (h, tempdiff1);
+      q_exact(k) = alpha(t(k)) + beta(t(k)) * intdu(t(k));
       q      = [q;q2];
       val_u(k,:) = u(x,t(k));
-      err(k) = max(abs(app_u(k,:) - val_u(k,:)));
+      err(k)     = max(abs(app_u(k,:) - val_u(k,:)));
     otherwise
-      v      = tau * tau * f(x,t(k - 1)) + 2 * app_u(k - 1,:);
-      coeff1 = (delta * delta) / q2; % tridiagonal system coefficients %
-      coeff2 = coeff1 / 6; % tridiagonal system coefficients %
-      b(:)   = -2 * (1 + coeff1 * (1 + coeff2));
+      v        = tau * tau * f(x,t(k - 1)) + 2 * app_u(k - 1,:);
+      coeff1   = (delta * delta) / q2; % tridiagonal system coefficients %
+      coeff2   = coeff1 / 6; % tridiagonal system coefficients %
+      b(:)     = -2 * (1 + coeff1 * (1 + coeff2));
       for i = 2:m
         phi(i) = -coeff2 * (v(i - 1) + 2 * (5 + coeff1) * v(i) + v(i + 1));
       endfor
       clear i
-      w(2:m) = tridiagonal_algotirhm (b, phi(2:m));
+      w(2:m)     = tridiagonal_algotirhm (b, phi(2:m));
       app_u(k,:) = w - app_u(k - 2,:);
       val_u(k,:) = u(x,t(k));
       err(k) = max(abs(app_u(k,:) - val_u(k,:)));
-      tempdiff1 = FinDiff (h, app_u(k,:));
-      tempdiff1 = tempdiff1 .* tempdiff1;
+      tempdiff1  = FinDiff (h, app_u(k,:));
+      tempdiff1  = tempdiff1 .* tempdiff1;
       %%% q integral %%%
-      q3     = alpha(t(k)) + beta(t(k)) * SimpsRule (h, tempdiff1);
+      q3         = alpha(t(k)) + beta(t(k)) * SimpsRule (h, tempdiff1);
+      q_exact(k) = alpha(t(k)) + beta(t(k)) * intdu(t(k));
       %%% Condition Number %%%
-      condN2 = 1 + (2 * eigcos) / (1 + coeff1 * (1 + coeff2) - eigcos);
+      condN2     = 1 + (2 * eigcos) / (1 + coeff1 * (1 + coeff2) - eigcos);
       %%%%%%%%%%%%%%%%%%%%%%%%
-      q      = [q;q3];
-      CondN2 = [CondN2;condN2]; %%% Condition Number %%%
+      q          = [q;q3];
+      CondN2     = [CondN2;condN2]; %%% Condition Number %%%
       q1 = q2;
       q2 = q3;
   endswitch
@@ -120,7 +123,8 @@ while k <= n + 1
 endwhile
 clear ('k');
 CondN2 = [CondN2;NaN]; %%% Condition Number %%%
-err_q  = abs(q - (alpha(t) + beta(t) * intdu(t)));
+
+max_rel_error_q = max(abs(1 - q ./ q_exact)); %%% maximum relative error %%%
 
 
 
@@ -185,7 +189,7 @@ save_as_csv (my_table, filecsv, dirname, cHeader);
 filemat  = sprintf('Results_Test%d_osc=%d_n=%d_m=%d.mat',problem,osc,n,m);
 matfile  = fullfile(dirname,filemat);
 save(matfile,"t","tau","x","h","delta","CondN2","val_u","app_u","q","err",...
-"err_q","ell","T","m","n","node_t","problem");
+"q_exact","max_rel_error_q","ell","T","m","n","node_t","problem");
 ##save(matfile,"t","tau","x","h","delta","CondN2","val_u","app_u","q","err",...
 ##"ell","T","m","n","problem");
 %%%%%%%%%%%%%%%%%%%%%%%%%
