e8f1b8a3e4d4d835e0810f5ce40320c0d6b9fc5a
Zurab Vashakidze <zurab.vashakidze@gmail.com>
2023-11-15 16:42:24 +0400

    We set the tolerance value to 0.01
    Co-Authored-By: Jemal Rogava <148445890+jemal-rogava@users.noreply.github.com>

--- Octave_Code/main_script_kirchhoff_unknown.m
+++ Octave_Code/main_script_kirchhoff_unknown.m
@@ -11,7 +11,7 @@ format longG
 
 tStart = cputime; % For CPU time determination %
 
-r     = 1; %%% Power of the tolerance, e.g. 10^(-r) %%%
+r     = 2; %%% Power of the tolerance, e.g. 10^(-r) %%%
 Tol   = sprintf('1e-%d',r);
 Tol   = str2double(Tol);
 
